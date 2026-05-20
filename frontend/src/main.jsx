26260c43d6e5b423645e75ee53cea73d4db71045
agungprabowospsi-dev <agungprabowospsi@gmail.com>
2026-05-20 15:19:58 +0700

    chore: bust vercel frontend cache

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,3 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 )
 // force rebuild Thu May  7 16:55:54 WIB 2026
 // watch path fixed Thu May  7 17:07:50 WIB 2026
+// force rebuild Wed May 20 15:19:58 WIB 2026
